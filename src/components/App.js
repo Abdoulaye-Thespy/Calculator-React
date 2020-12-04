d95877ef956dd1b6721bf84e9ae8b75bf16db31e
Abdoulaye-Thespy <njigouhrazak@iut-dhaka.edu>
2020-12-04 15:47:43 +0100

    Fixed App component and display component

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,39 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+import calculate from '../logic/calculate';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      // total: null,
-      // next: null,
-      // operation: null,
+      total: null,
+      next: null,
+      operation: null,
     };
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick() {
+  handleClick(ButtonName) {
+    const data = calculate(this.state, ButtonName);
+    const { totals, nexts, operations } = data;
     this.setState({
+      total: totals,
+      next: nexts,
+      operation: operations,
     });
-    // Change code above this line
   }
 
   render() {
+    /* eslint-disable */ 
+    const { total, next, operation } = this.state;
+    const result = next || total || '0';
+ /* eslint-enable */
     return (
       <div>
+
         <>
-          <Display />
+          <Display total={result.toString()} />
           <ButtonPanel />
         </>
       </div>
