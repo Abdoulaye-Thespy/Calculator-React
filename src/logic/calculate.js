4efa9a1d16a7ce09a3385674ce097690f373fca2
Abdoulaye-Thespy <njigouhrazak@iut-dhaka.edu>
2020-12-06 20:16:33 +0100

    linters errors

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -2,7 +2,7 @@ import operate from './operate';
 
 function calculate(calculatorData, buttonName) {
   let { total, next, operation } = calculatorData;
-  const operators = ['+', 'x', '-', '÷'];
+  const operators = ['+', 'X', '-', '÷'];
   const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
   if (buttonName === 'AC') {
